7dc6bb42410b603f180f2d746406c6f341f3358a
Yasser Bashir <yasser.bashir@arbisoft.com>
2017-12-07 11:26:35 +0500

    Added support for local settings

--- settings.py
+++ settings.py
@@ -1,11 +1,16 @@
 import googlemaps
 
-GOOGLE_MAPS_KEY = 'Add you own google maps key here'
-gmaps_client = googlemaps.Client(key=GOOGLE_MAPS_KEY)
+GOOGLE_MAPS_KEY = 'Add your own google maps key here or in a local_settings file'
 
 MAX_LOCATIONS = 25
 SEARCH_RADIUS = 25000
 LIMIT_LOCATIONS = 11
 
+try:
+  import local_settings
+  GOOGLE_MAPS_KEY = local_settings.GOOGLE_MAPS_KEY
+except:
+  pass
 
+gmaps_client = googlemaps.Client(key=GOOGLE_MAPS_KEY)
 
