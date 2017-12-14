f450e611c0a9e3d4d161ab0aa25b06bb4e5ed9bb
Yasser Bashir <yasser.bashir@arbisoft.com>
2017-12-14 18:17:36 +0500

    Some more code conservation

--- solution.py
+++ solution.py
@@ -1,19 +1,23 @@
-import itertools,sys
+import itertools, sys
 from fetch import *
 from plot import draw_map
 
 distances = []
 
+
 def distance(x, y):
   return distances[x][y]
+
 
 def route_length(route):
   """Distance between first and last and consecutive elements of a list."""
   return sum(distance(route[i], route[i - 1]) for i, v in enumerate(route))
 
+
 def all_routes(seq):
   """Return all permutations of a list, each starting with the first item"""
   return [[seq[0]] + list(rest) for rest in itertools.permutations(seq[1:])]
+
 
 def all_partitions(collection):
   """Returns the set of all partitions for a given set
@@ -29,22 +33,20 @@ def all_partitions(collection):
       yield smaller[:n] + [[first] + subset] + smaller[n + 1:]
     yield [[first]] + smaller
 
-def shortest_route(routes):
-  """Given a list of routes returns the minimum based on route length"""
-  return min(routes, key=route_length)
-
 def k_partitions_with_shortest_routes(ids, k=1):
   """Our partitions represent number of vehicles. This function yields
      an optimal path for each vehicle given the destinations assigned to it"""
-  for p in filter(lambda x: len(x) == k,all_partitions(ids[1:])):
-      yield [shortest_route(all_routes([ids[0]] + q)) for q in p]
+  for p in filter(lambda x: len(x) == k, all_partitions(ids[1:])):
+    yield [min(all_routes([ids[0]] + q), key=route_length) for q in p]
 
-def shortest_partition(loc_ids, partitions=1):
+
+def shortest_partition(loc_ids, k=3):
   """This function receives all k-subsets of a route and returns the subset
     with minimum distance cost. Note the total time is always equal to
     the max time taken by any single vehicle"""
-  return min(k_partitions_with_shortest_routes(loc_ids, partitions),
-             key=lambda x: max(route_length(x[i]) for i in range(partitions)))
+  return min(k_partitions_with_shortest_routes(loc_ids, k),
+             key=lambda x: max(route_length(x[i]) for i in range(k)))
+
 
 if __name__ == '__main__':
   search_query = sys.argv[1]
@@ -55,13 +57,13 @@ if __name__ == '__main__':
   distances = get_distances(locations, search_query)
   t0 = time.clock()
   shortest_route = shortest_partition(location_ids, num_vehicles)
-  paths = get_paths(locations,shortest_route)
+  paths = get_paths(locations, shortest_route)
 
   draw_map(locations, [], "input.html")
   draw_map(locations, paths, 'output.html')
 
-  with open('output.txt','a') as file:
+  with open('output.txt', 'a') as file:
     file.write('Solution time: {0:.2f} seconds\n'.format(time.clock() - t0))
     file.write('Shortest route time: {0:.1f} minutes\n'.
-        format(max(route_length(i) for i in shortest_route)))
+               format(max(route_length(i) for i in shortest_route)))
     file.write('Shortest route is: {0}\n'.format(shortest_route))
